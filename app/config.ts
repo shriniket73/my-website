efc87c6325aa5a4bba803bc38483e4781a8beca9
Shriniket Acharya <shriniketacharya@gmail.com>
2024-10-26 02:22:04 +0530

    New website with vercel template

--- app/config.ts
+++ app/config.ts
@@ -1,16 +1,16 @@
 export const metaData = {
   baseUrl: "https://nextfolio-template.vercel.app/",
-  title: "Nextfolio",
-  name: "Sirius",
+  title: "Shriniket",
+  name: "Shriniket",
   ogImage: "/opengraph-image.png",
   description:
-    "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
+    "This is my brian on internet",
 };
 
 export const socialLinks = {
-  twitter: "https://x.com/1tssirius",
-  github: "https://github.com/1msirius/Nextfolio",
+  twitter: "https://x.com/shriniket73",
+  github: "https://github.com/shriniket73",
   instagram: "https://www.instagram.com/",
-  linkedin: "https://www.linkedin.com/",
-  email: "mailto:example@gmail.com",
+  linkedin: "https://www.linkedin.com/in/shriniket-acharya/",
+  email: "mailto:shriniketacharya@gmail.com",
 };
--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -7,18 +7,18 @@ export interface Project {
 
 export const projects: Project[] = [
   {
-    title: "Project One",
-    year: 2023,
+    title: "Sales Question Generator MVP",
+    year: 2024,
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam veritatis dolorem rem praesentium dicta labore, at laudantium quisquam.",
-    url: "https://example.com/",
+      "Created a quick MVP, that emails you Sales Discovery question about any company from their website. Add your email and company website and it emails you set of question to gets started for your SDRs",
+    url: "https://www.linkedin.com/posts/shriniket-acharya_in-this-video-i-share-how-simple-it-is-to-activity-7246877589754052608-4NRs?utm_source=share&utm_medium=member_desktop",
   },
   {
-    title: "Project Two",
-    year: 2022,
+    title: "Calorie and Protein Calculator",
+    year: 2024,
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam veritatis dolorem rem praesentium dicta labore, at laudantium quisquam.",
-    url: "https://example.com/",
+      "Created an app that takes your food photo and provides you total and item wise calories and protein information",
+    url: "https://www.linkedin.com/posts/shriniket-acharya_in-this-video-i-talk-about-two-things-activity-7255360993629765634-VrJM?utm_source=share&utm_medium=member_desktop",
   },
   {
     title: "Project Three",
